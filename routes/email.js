a7286c117e63aa6e4f507ce7b2ce0a7a7309cf5a
NeelGoel <neelkanth.goel@designit.com>
2022-08-02 16:11:24 +0530

    adding node mailer

--- routes/email.js
+++ routes/email.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const router = express.Router();
+var nodemailer = require('nodemailer');
 
-router.get("/", async(req, res) => {
+router.post("/", async(req, res) => {
     try{
-        res.send('hello')
+        var gmailPassword = {
+            query: 'kynkkpidulwletai'
+        }
+        
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: req.body.from,
+              pass: gmailPassword[req.body.password]
+            }
+          });
+          
+          var mailOptions = {
+            from: req.body.from,
+            to: req.body.to,
+            subject: req.body.subject,
+            html: `<h1>Hi ${req.body.name}</h1>
+                    </br></br>
+                    <p>${req.body.body}</p>
+                    </br>
+                    <b>Your Query:</b>
+                    <div><p>${req.body.desc}</p></div>
+                    </br>
+                    </br>
+                    <p>Thanks & Regards</p>
+                    <div><p>Next Universe</p><div>
+                    `
+          };
+          
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+              res.json({name: req.body.name, result:'Email has been sent'});
+            }
+          });
     }
     catch(error) {
     console.error(error);
