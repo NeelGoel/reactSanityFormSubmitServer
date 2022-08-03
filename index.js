a6480f5b8dd096fcf356efdb94cdd9ca594676e7
NeelGoel <neelkanth.goel@designit.com>
2022-08-03 12:09:48 +0530

    adding initial server hit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const email = require('./routes/email');
+const server = require('./routes/server');
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json({extend: false}));
+app.use("/", server);
 app.use("/sendemail", email);
 
 const PORT = process.env.PORT || 8080;
